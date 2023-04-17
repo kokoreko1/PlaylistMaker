68e439283580233fa924b48d09b5ec15c7db8ef1
kokoreko1 <kokoreko@yandex.ru>
2023-04-17 17:01:00 +0300

    002

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        ///////////////////////////////
+        // кнопка Поиск
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+
+        buttonSearch.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+//        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        buttonSearch.setOnClickListener(buttonSearchClickListener)
+//
+//        // лямбда
+//        buttonSearch.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск (лямбда)", Toast.LENGTH_SHORT).show()
+//        }
+
+        ///////////////////////////////
+        // кнопка Медиатека
+        val buttonLibrary = findViewById<Button>(R.id.button_library)
+
+        buttonLibrary.setOnClickListener {
+            val displayIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+//        val buttonLibraryClickListener: View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        buttonLibrary.setOnClickListener(buttonLibraryClickListener)
+//
+//        // лямбда
+//        buttonMedia.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека (лямбда)", Toast.LENGTH_SHORT).show()
+//        }
+
+        ///////////////////////////////
+        // кнопка Настройка
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        buttonSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+//        buttonSettings.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки (лямбда)", Toast.LENGTH_SHORT).show()
+//        }
+
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,24 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_settings)
+
+        val imageBack = findViewById<ImageView>(R.id.image_back)
+
+        imageBack.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+    }
+}
