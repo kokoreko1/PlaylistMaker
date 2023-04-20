2f24d421f4e69e7a5afafa2ec65cf0c80d47bb62
kokoreko1 <kokoreko@yandex.ru>
2023-04-20 12:59:50 +0300

    Спринт 9/16 → Тема 2/5: Context и Intent → Урок 3/3

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -20,6 +19,65 @@ class SettingsActivity : AppCompatActivity() {
 
         imageBack.setOnClickListener {
             finish()
+        }
+
+        /////////////////////////////////////////
+        // кнопка Поделиться приложением
+        val imageShare = findViewById<ImageView>(R.id.image_share)
+
+        imageShare.setOnClickListener {
+
+            val androidDeveloper = "https://practicum.yandex.ru/android-developer/"
+
+            val shareIntent = Intent()
+
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type = "text/plain"
+
+            shareIntent.putExtra(Intent.EXTRA_TEXT, androidDeveloper)
+
+            startActivity(Intent.createChooser(shareIntent,"Share via"))
+
+        }
+
+        /////////////////////////////////////////
+        // кнопка Поделиться приложением
+        val imageSupport = findViewById<ImageView>(R.id.image_support)
+
+        imageSupport.setOnClickListener {
+
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение."
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+
+            val shareIntent = Intent()
+
+            shareIntent.action = Intent.ACTION_SENDTO
+
+            shareIntent.data = Uri.parse("mailto:")
+
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("kokoreko@ya.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+
+            startActivity(shareIntent)
+
+        }
+
+        /////////////////////////////////////////
+        // кнопка Пользовательское соглашение
+        val imageAgreement = findViewById<ImageView>(R.id.image_agreement)
+
+        imageAgreement.setOnClickListener {
+
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+
+            val shareIntent = Intent()
+
+            shareIntent.action = Intent.ACTION_VIEW
+            shareIntent.data = url
+
+            startActivity(shareIntent)
+
         }
 
     }
