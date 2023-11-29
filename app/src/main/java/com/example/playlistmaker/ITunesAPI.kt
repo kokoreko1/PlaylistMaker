ff404e9ce80ffb6572aae4356662c45767c92a02
kokoreko1 <kokoreko@yandex.ru>
2023-11-29 20:27:59 +0300

    Спринт 11, изменения 01

---
+++ app/src/main/java/com/example/playlistmaker/ITunesAPI.kt
@@ -0,0 +1,14 @@
+package com.example.playlistmaker
+
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Call
+
+interface ItunesAPI {
+
+    @GET("/search?entity=song")
+    fun search(@Query("term") text: String): Call<ITunesResponse>
+
+
+}
